0dd4090efb7e1d0f27a8fa3b4398bce4842eae5e
vbondar-hub <v.bondar@igrudsky.com>
2026-04-21 00:42:55 +0300

    fix name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import MapSection from "@/components/MapSection";
 export default function Home() {
   return (
     <main className="mx-auto max-w-6xl p-6">
-      <h1 className="mb-6 text-3xl font-bold">Лабораторна робота №2</h1>
+      <h1 className="mb-6 text-3xl font-bold">Лабораторна робота №4</h1>
 
       <section className="space-y-4">
-        <h2 className="text-xl font-semibold">Інтерактивна карта</h2>
+        <h2 className="text-xl font-semibold">
+          Оптимізація та деплой продакшн-застосунку
+        </h2>
         <MapSection />
       </section>
     </main>
